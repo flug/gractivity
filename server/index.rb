9146e159806372a4928151355aee075220ce45ed
Flug <flugv1@gmail.com>
2014-01-11 00:47:16 +0100

    declare SassHandler for convert sass to css

--- server/index.rb
+++ server/index.rb
@@ -7,4 +7,13 @@ require 'yaml'
 require 'sass'
 require 'coffee-script'
 
+class SassHandler < Sinatra::Base
+
+	set :views, File.dirname(__FILE__)+ '/assets/stylesheets/sass'
+	get '/css/*.css' do
+		filename = params[:splat].first
+		sass filename.to_sym
+	end
+
+end
 
