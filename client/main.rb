3f4fb2fa641c365124aca2b7d01f369d369acf1e
Flug <flugv1@gmail.com>
2014-01-12 16:24:02 +0100

    add method for save configuration

--- client/main.rb
+++ client/main.rb
@@ -16,8 +16,37 @@ Shoes.app :title => "Gractivity",  :width => 300 , :height => 170 ,  :resizable 
 			@list = list_box :items => ['Gnome', 'KDE']
 		end
 
+		flow :top  => 75  do
+
+			para "Enter you token"
+			@token  = edit_line
+			button "Save" do
+				if not @token.text.empty?  || @list.text.empty?
+					begin
+
+						if @list.text == 'Gnome'
+							activity =  GNOME
+						else
+							activity = KDE
+						end
+						@prepare = Hash.new
+						@prepare['configuration']  =  { 'token' =>  @token.text, 'activity' => activity }
+
+						file = File.open(File.expand_path(CONFIGURATION_FILE), "w")
+						file.write @prepare.to_yaml
+					rescue IOError => e
+						alert "Error on save you profile configuration"
+					ensure
+						file.close unless file == nil
+					end
+				else
+					alert "you must specify the configuration"
+				end
+			end
+		end
 	end
-	stack :margin => 12 , :height => 50  do
+
+	stack :margin => 12 , :height => 50, :top  => 120   do
 		flow  do
 			button "Backup" do
 			end
