390ceb27f100505c60e5fdc70983151121abe64d
Flug <flugv1@gmail.com>
2014-01-12 16:22:08 +0100

    move element

--- client/main.rb
+++ client/main.rb
@@ -8,20 +8,9 @@ Shoes.app :title => "Gractivity",  :width => 300 , :height => 170 ,  :resizable 
 
 	KDE = ".kde"
 	GNOME = ".gnome"
+	stack :margin => 12 do
 
-	stack :margin => 12 do 
-		para "Enter you token"
-		flow do
-			edit_line 
-			button "Save" do
-			end
-		end
-		
-
-	end
-
-	stack :margin => 12, :height => 50 do
-		flow do
+		flow  do
 
 			@list = list_box :items => ['Gnome', 'KDE']
 		end
