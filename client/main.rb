4e7100e904ff4c56c3cb3b699ca8298d4dd36797
Flug <flugv1@gmail.com>
2014-01-13 23:32:07 +0100

    implement compression tar.gz directory

--- client/main.rb
+++ client/main.rb
@@ -19,32 +19,46 @@ Shoes.app :title => "Gractivity",  :width => 300 , :height => 180 ,  :resizable 
 		flow  do
 			para "Choice you activity"
 			@list = list_box :items => ['Gnome', 'KDE']
-		end
+			activity = configuration.activity
+			if not activity.empty?
+			end
 
-		flow :top  => 75  do
+			flow :top  => 75  do
 
-			para "Enter you token"
-			@token  = edit_line
-			button "Save" do
-				if not @token.text.empty?  || @list.text.empty?
+				para "Enter you token"
+				@token  = edit_line
+				if not configuration.token.empty?
+					@token.text = configuration.token
+				end
 
-					if @list.text == 'Gnome'
-						activity =  GNOME
-					else
-						activity = KDE
+				button "Save" do
+					if not @token.text.empty?  || @list.text.empty?
+
+						if @list.text == 'Gnome'
+							activity =  GNOME
+						else
+							activity = KDE
+						end
+						configuration.save @token.text, activity
 					end
-					configuration.save @token.text, activity
 				end
 			end
-		end
 
-		stack :margin => 12 , :height => 50, :top  => 120   do
-			flow  do
-				button "Backup" do
-				end
-				button "Restore", :left => 70  do
-				end
+			stack :margin => 12 , :height => 50, :top  => 120   do
+				flow  do
+					button "Backup" do
+						if configuration.exist?
+							directory  = File.expand_path '~/'+ activity
+							zipfile_name  = '/tmp/'+ activity[1, activity.length] +'.tgz'
+							alert "Save end"  if  system('tar cvzf '+zipfile_name  +' '+directory)
 
+						end
+					end
+					button "Restore", :left => 70  do
+					end
+
+				end
 			end
 		end
 	end
+end
